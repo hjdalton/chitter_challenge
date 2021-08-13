c0be22227cb70d30ea2fd8e07a7cfaf6f0b1b7ab
hjdalton <hjdalton@hotmail.co.uk>
2021-08-13 10:11:43 +0100

    classes, views and test files created from diagram

--- app.rb
+++ app.rb
@@ -5,5 +5,9 @@ class Chitter < Sinatra::Base
     'Test page'
   end
 
+  get '/' do
+    erb :index
+  end
+
   run! if app_file == $0
 end
---
+++ spec/features/show_message_spec.rb
@@ -0,0 +1,9 @@
+feature 'shows the users messages' do
+  scenario 'a list of messages appears' do
+    connection = PG.connect(dbname: 'chitter_test')
+    Messages.show
+    visit('/')
+    
+    expect(page).to have_content("This is a peep!")
+  end
+end
